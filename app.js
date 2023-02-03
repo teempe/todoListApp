e4bc9f1d4a15429cdb96d9d12823cb0d4fa652d5
Tomasz <tomasz.pe@interia.eu>
2023-02-03 17:55:30 +0100

    add base setup for handlebars

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {});
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+    res.render('todolist');
+});
 
 app.listen(3000, 'localhost', () => console.log('Server listens on port 3000'));
