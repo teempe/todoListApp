0b9124e981799f1e7a7d478c8dcd17e5885a4d36
Tomasz <tomasz.pe@interia.eu>
2023-02-04 16:15:29 +0100

    add counter of active items

--- public/js/front.js
+++ public/js/front.js
@@ -7,7 +7,17 @@ const filterCompleted = document.querySelector('a[href="/completed"]');
 const filterAll = document.querySelector('a[href="/"]');
 const selectAll = document.querySelector('label[for="toggle-all"]');
 const clearButton = document.querySelector('button.clear-completed');
+const counter = document.querySelector('span.todo-count');
 
+
+const countItemsLeft = () => {
+    const notCompletedItems = [ ...document.querySelectorAll('li[data-id]') ]
+            .filter(task => !task.classList.contains('completed'))
+            .length;
+    
+    const message = `<strong>${notCompletedItems}</strong> ${notCompletedItems > 1 ? 'items' : 'item'} left`;
+    counter.innerHTML = message;
+}
 
 const fetchData = async (data, method, path) => {
     const response = await fetch(path, {
@@ -50,10 +60,11 @@ const toggleTaskCompleted = async event => {
 
     const data = getTaskDetails(taskId);
     await fetchData(data, 'PUT', '/');
+    countItemsLeft();
 }
 
 const toggleSelection = async event => {
-    const allTasks = [...document.querySelectorAll('li[data-id]')];
+    const allTasks = [ ...document.querySelectorAll('li[data-id]') ];
 
     const isCompleted = task => task.classList.contains('completed');
     const isNotCompleted = task => !task.classList.contains('completed');
@@ -153,3 +164,5 @@ filterCompleted.addEventListener('click', filterCompletedTasks);
 selectAll.addEventListener('click', toggleSelection);
 
 clearButton.addEventListener('click', deleteSelectedTasks);
+
+countItemsLeft();
